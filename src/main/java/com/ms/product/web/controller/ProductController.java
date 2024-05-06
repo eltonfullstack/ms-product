2dec20219f2291bf296a90ebee576444f7393594
Elton Silva <eltonfullstack@gmail.com>
2024-05-05 22:50:32 -0300

    Entity, Repository, Service and Controller

---
+++ src/main/java/com/ms/product/web/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.ms.product.web.controller;
+
+import com.ms.product.entity.Product;
+import com.ms.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> save(Product product) {
+        Product createdProduct = productService.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> products = productService.findAll();
+        return ResponseEntity.status((HttpStatus.OK)).body(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(Long id) {
+        Product product = productService.findById(id);
+        return ResponseEntity.status((HttpStatus.OK)).body(product);
+    }
+}
